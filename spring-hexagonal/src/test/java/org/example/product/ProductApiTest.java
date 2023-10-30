4552219bd1f2a96261c14b5a19e8910d7e02701a
good0619@naver.com <32181771@dankook.ac.kr>
2023-10-31 01:49:47 +0900

    test : data select logic api test

--- spring-hexagonal/src/test/java/org/example/product/ProductApiTest.java
+++ spring-hexagonal/src/test/java/org/example/product/ProductApiTest.java
@@ -16,18 +16,37 @@ public class ProductApiTest extends ApiTest {
 
     @Test
     @DisplayName("상품 등록")
-    void 상품등록() {
+    void 상품_등록() {
         final var request = AddProductRequestFixture.get();
-
         //API 요청
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        final var response = 상품등록(request);
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @DisplayName("상품 조회")
+    void 상품_조회() {
+        상품등록(AddProductRequestFixture.get());
+
+        final Long productId = 1L;
+
+        final var response = RestAssured.given().log().all()
+                .get("/products/{productId}", productId)
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getString("name")).isEqualTo("name");
+    }
+
+    private static ExtractableResponse<Response> 상품등록(AddProductRequest request) {
+        return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(request)
                 .when()
                 .post("/products")
                 .then()
                 .log().all().extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 }

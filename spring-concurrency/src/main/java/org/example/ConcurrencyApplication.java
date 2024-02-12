31eb481d0ba143f3e179b57e9e50dbf17c2f920f
good0619@naver.com <32181771@dankook.ac.kr>
2024-02-12 22:46:53 +0900

    feat : connect database

--- spring-concurrency/src/main/java/org/example/ConcurrencyApplication.java
+++ spring-concurrency/src/main/java/org/example/ConcurrencyApplication.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ConcurrencyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
 }

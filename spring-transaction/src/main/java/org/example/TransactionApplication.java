5a4cd202f5bec91b4488161cabfa849488f397f8
YGwan <good0619@naver.com>
2024-04-17 00:51:47 +0900

    feat : apply MyTransactional annotation

--- spring-transaction/src/main/java/org/example/TransactionApplication.java
+++ spring-transaction/src/main/java/org/example/TransactionApplication.java
@@ -2,19 +2,19 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.example.aop.TransactionAspect;
 import org.example.entity.User;
 import org.example.service.MyTransactionalService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
+@Import(TransactionAspect.class)
 @SpringBootApplication
 public class TransactionApplication {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(TransactionApplication.class);
-        application.setAdditionalProfiles("dev");
-
         ApplicationContext ac = SpringApplication.run(TransactionApplication.class, args);
         List<User> users = new ArrayList<>();
         users.add(new User(1L, "YongGwan", 26));

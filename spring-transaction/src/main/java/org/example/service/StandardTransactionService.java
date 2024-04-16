a354160970885ffc70e18bb399bef76a74c668a4
YGwan <good0619@naver.com>
2024-04-16 21:32:27 +0900

    feat : implement @Transactional Anotation logic

---
+++ spring-transaction/src/main/java/org/example/service/StandardTransactionService.java
@@ -0,0 +1,30 @@
+package org.example.service;
+
+import java.sql.SQLException;
+import java.util.List;
+import org.example.dao.UserDao;
+import org.example.entity.User;
+import org.springframework.transaction.annotation.Transactional;
+
+public class StandardTransactionService {
+
+    private final UserDao userDao;
+
+    public StandardTransactionService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    /**
+     * declarative transaction
+     */
+    @Transactional
+    public void joinAllUser(List<User> users) {
+        for (User user : users) {
+            try {
+                userDao.joinUser(user);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}

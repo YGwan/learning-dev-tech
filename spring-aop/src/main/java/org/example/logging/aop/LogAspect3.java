19bae754d8d25037a8964f67c63005017c8e59d1
YGwan <good0619@naver.com>
2024-04-24 01:13:51 +0900

    refactor : AOP Version 3 refactoring

--- spring-aop/src/main/java/org/example/logging/aop/LogAspect3.java
+++ spring-aop/src/main/java/org/example/logging/aop/LogAspect3.java
@@ -1,9 +1,6 @@
 package org.example.logging.aop;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -13,10 +10,11 @@ import org.aspectj.lang.annotation.Pointcut;
 public class LogAspect3 {
 
     @Pointcut("execution(* org.example.service..*(..))")
-    private void logServicePoint(){}
+    private void logServicePoint() {
+    }
 
     @Before("logServicePoint()")
-    public void doLogging(JoinPoint joinPoint) {
+    public void doLogging() {
         System.out.println("aop test v3");
         log.info("aop logging test v3");
     }

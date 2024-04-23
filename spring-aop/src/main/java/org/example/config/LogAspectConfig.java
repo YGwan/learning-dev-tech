9ecaa57ec92c6490118aa7bb7426f7758de36208
YGwan <good0619@naver.com>
2024-04-24 01:08:42 +0900

    feat : implement manage LogAspect bean in Config file

---
+++ spring-aop/src/main/java/org/example/config/LogAspectConfig.java
@@ -0,0 +1,20 @@
+package org.example.config;
+
+import org.example.logging.aop.LogAspect1;
+import org.example.logging.aop.LogAspect2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogAspectConfig {
+
+    @Bean
+    public LogAspect1 logAspect1() {
+        return new LogAspect1();
+    }
+
+    @Bean
+    public LogAspect2 logAspect2() {
+        return new LogAspect2();
+    }
+}
--- spring-aop/src/main/java/org/example/logging/aop/LogAspect1.java
+++ spring-aop/src/main/java/org/example/logging/aop/LogAspect1.java
@@ -4,9 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.stereotype.Component;
 
-@Component
 @Slf4j
 @Aspect
 public class LogAspect1 {

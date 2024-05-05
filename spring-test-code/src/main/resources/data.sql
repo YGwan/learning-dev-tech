0c6a7c9c95960b22f446f14d2099edf8d2aee648
YGwan <good0619@naver.com>
2024-05-06 01:18:54 +0900

    feat : implement data.sql

---
+++ spring-test-code/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into product(id, product_number, product_type, product_status, name, price, created_time, updated_time)
+values (1, '001', 'HANDMADE', 'SELLING', '아메리카노', 4000, now(), now()),
+       (2, '002', 'HANDMADE', 'HOLD', '카페라떼', 4500, now(), now()),
+       (3, '003', 'BAKERY', 'STOP_SELLING', '크루아상', 3500, now(), now());

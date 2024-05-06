9360d5487bfe023c60f8e2423a6b1dc26273f900
YGwan <good0619@naver.com>
2024-05-07 02:02:54 +0900

    feat : add stock save test

--- spring-test-code/src/test/java/org/example/repository/StockRepositoryTest.java
+++ spring-test-code/src/test/java/org/example/repository/StockRepositoryTest.java
@@ -1,5 +1,6 @@
 package org.example.repository;
 
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.example.domain.Stock;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,7 +18,25 @@ class StockRepositoryTest {
     @Autowired
     private StockRepository stockRepository;
 
-    @DisplayName("상품 번호 리스트로ㄷ 따른 재고를 조회한다,")
+    @DisplayName("재고 생성 후 잘 저장되는지 확인한다")
+    @Test
+    void createStocks() {
+        var stock1 = Stock.create("001", 2);
+        var stock2 = Stock.create("002", 5);
+
+        stockRepository.saveAll(List.of(stock1, stock2));
+
+        var stocks = stockRepository.findAll();
+
+        assertThat(stocks).hasSize(2)
+                .extracting("productNumber", "quantity")
+                .containsExactlyInAnyOrder(
+                        AssertionsForClassTypes.tuple("001", 2),
+                        AssertionsForClassTypes.tuple("002", 5)
+                );
+    }
+
+    @DisplayName("상품 번호 리스트로 따른 재고를 조회한다.")
     @Test
     void test() {
         var stock1 = Stock.create("001", 2);

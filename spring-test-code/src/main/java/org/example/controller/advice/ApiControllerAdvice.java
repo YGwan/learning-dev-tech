15b358299c46e459e3e9cc0b71c0e5844121b0e0
YGwan <good0619@naver.com>
2024-05-10 15:39:28 +0900

    fix : controllerAdvice -> RestControllerAdvice

--- spring-test-code/src/main/java/org/example/controller/advice/ApiControllerAdvice.java
+++ spring-test-code/src/main/java/org/example/controller/advice/ApiControllerAdvice.java
@@ -3,15 +3,17 @@ package org.example.controller.advice;
 import org.example.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ApiControllerAdvice {
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BindException.class)
     public ApiResponse<Object> bindException(BindException e) {
         String message = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        return ApiResponse.of(HttpStatus.BAD_REQUEST, message);
+        return ApiResponse.of(HttpStatus.BAD_REQUEST, message, null);
     }
 }

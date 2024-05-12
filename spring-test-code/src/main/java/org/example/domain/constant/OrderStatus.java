50e7f6387356f466289cabef08e5d3396d4ff026
YGwan <good0619@naver.com>
2024-05-12 23:56:13 +0900

    feat : add order status

--- spring-test-code/src/main/java/org/example/domain/constant/OrderStatus.java
+++ spring-test-code/src/main/java/org/example/domain/constant/OrderStatus.java
@@ -9,7 +9,11 @@ public enum OrderStatus {
 
     INIT("주문생성"),
     CANCELED("주문취소"),
-    RECEIVED("주문접수");
+    RECEIVED("주문접수"),
+    PAYMENT_COMPLETED("결제 완료"),
+    PAYMENT_FAIL("결제 실패"),
+    COMPLETED("처리 완료")
+    ;
 
     private final String text;
 }

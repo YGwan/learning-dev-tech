732e5e09a5066c0ad6ab913a87e07a03e82949e7
YGwan <good0619@naver.com>
2024-05-15 17:46:12 +0900

    feat : unit test - used BDDMockito  & @Mock

--- spring-test-code/src/test/java/org/example/service/mail/MailServiceTest4.java
+++ spring-test-code/src/test/java/org/example/service/mail/MailServiceTest4.java
@@ -1,44 +1,57 @@
 package org.example.service.mail;
 
+import org.example.domain.MailSendHistory;
+import org.example.repository.MailSendHistoryRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
- * 통합테스트 - springboot 사용
+ * 단위테스트 - @Mock, @InjectMocks Mocking
+ * BDDMockito 사용
  */
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class MailServiceTest4 {
 
-    @Autowired
-    private MailService mailService;
-
-    @MockBean
+    @Mock
     private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
 
     @DisplayName("메일 전송이 정상적으로 완료되면 true값이 반환된다.")
     @Test
     void sendMail() {
-        when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(true);
+//        Mockito.when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+//                .thenReturn(true);
+
+        BDDMockito.given(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(true);
 
         boolean result = mailService.sendMail("fromEmail", "email", "subject", "content");
 
         assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
     }
 
     @DisplayName("메일 전송이 실패하면 IllegalArgumentException 에러를 발생시킨다.")
     @Test
     void sendMailWhenFail() {
-        when(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
-                .thenReturn(false);
+        BDDMockito.given(mailSendClient.sendMail(anyString(), anyString(), anyString(), anyString()))
+                .willReturn(false);
 
         var subject = "subject";
 
